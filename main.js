0195e54950434703014a1cf18271718c9c47e55c
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-09-06 18:08:02 +0300

    add opacity vertically line

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 let buttons = document.querySelectorAll('.btn');
 
 buttons.forEach((btn) => {
+  let vertically = btn.querySelector('.vertically');
   btn.addEventListener('click', function () {
     let target = this.dataset.target;
     let targetElement = document.getElementById(target);
+
     targetElement.classList.toggle('open');
 
-
+    if (targetElement.classList.contains('open')) {
+      vertically.style.opacity = '0';
+    } else {
+      vertically.style.opacity = '1';
+    }
 
     document.addEventListener('click', (e) => {
       if (!e.target.closest('.marker')) {
         targetElement.classList.remove('open');
+        vertically.style.opacity = '1';
       }
     });
 
