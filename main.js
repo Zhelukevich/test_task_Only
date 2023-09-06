ff985f5d3a041d396201bfa7e78988b9a3dc37cd
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-09-06 17:57:24 +0300

    add layout btn and add class open

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log(1);
+let buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', function () {
+    let target = this.dataset.target;
+    let targetElement = document.getElementById(target);
+    targetElement.classList.toggle('open');
+
+
+
+    document.addEventListener('click', (e) => {
+      if (!e.target.closest('.marker')) {
+        targetElement.classList.remove('open');
+      }
+    });
+
+  })
+
+
+})
